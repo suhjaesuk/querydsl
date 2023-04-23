de45675dabde03397322c58a1ba9a18911168780
suhjaesuk <suhjaesuk@gmail.com>
2023-04-23 13:56:28 +0900

    Case문 테스트 케이스 추가

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -386,4 +387,38 @@ public class QuerydslBasicTest {
             System.out.println("tuple = " + tuple);
         }
     }
+    
+    @Test
+    public void basicCase() {
+        List<String> result = queryFactory.select(member.age
+                .when(10).then("열살")
+                .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);;
+        }
+    }
+
+    /**
+     * DB 에선 Raw 데이터만.
+     * DB 에서 불러오지말고 어플리케이션 단에서 해결하자.
+     */
+    @Test
+    public void complexCase() {
+        List<String> result = queryFactory.select(new CaseBuilder()
+                .when(member.age.between(0,20)).then("0~20살")
+                .when(member.age.between(21,30)).then("21~30살")
+                .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
 }
